732845972353c1da6e8ddddf631d1f427d33d943
Rifai Kuçi <rifai.kuci@finastech.com>
2022-09-17 11:26:10 +0300

    file fix

--- common/methods/file.php
+++ common/methods/file.php
@@ -100,6 +100,8 @@ function pdfUpload( $folderName, $name, $fileName)
         return "pdf_invalid_type";
         $uploadOk = 0;
     }
+    echo move_uploaded_file($_FILES[$name]["tmp_name"], "../" . $target_file);
+    exit();
 
     if ($uploadOk == 0) {
         return "pdf_not_upload";
