9f5c7d7d1fa241b189da15f5edda1d0893d83f8d
Rifai Kuçi <rifai.kuci@finastech.com>
2022-09-24 09:57:48 +0300

    target change

--- common/data/skills.php
+++ common/data/skills.php
@@ -1,7 +1,7 @@
 <?php
 
 if($lang == 'tr') {
-    $TYPE_SKILLS = ["programming" => "Programlama Dilleri", "db"=>"Veritabanı", "editor" => "Editörler", "hobies" => "Hobiler", "framework" => "Framework"];
+    $TYPE_SKILLS = ["programming" => "Programlama Dilleri", "db"=>"Veritabanı", "editor" => "Editörler", "hobies" => "Hobiler", "other" => "Diğer"];
 } else if($lang == 'en') {
-    $TYPE_SKILLS = ["programming" => "Programming Languages", "db"=>"Database", "editor" => "Editors", "hobies" => "Hobbies", "framework" => "Framework"];
+    $TYPE_SKILLS = ["programming" => "Programming Languages", "db"=>"Database", "editor" => "Editors", "hobies" => "Hobbies", "other" => "Other"];
 }
