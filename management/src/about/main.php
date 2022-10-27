d1affee4a6d902ebc1cb5fc83fdf0f595b0fb3bf
Rifai Kuçi <rifai.kuci@finastech.com>
2022-10-27 17:41:59 +0300

    edit

--- management/src/about/main.php
+++ management/src/about/main.php
@@ -24,7 +24,7 @@ $row = getDataRow(1, 'about', $db);
                     <?php
                     getTextInput(4, "Title", "", "title", $row['title'], false, false);
                     getTextInput(5, "Link", "", "link", $row['link'], false, false);
-                    getTextArea(12, "Hakkımda", '', "about", $row['about'], false, false,false);
+                    getTextArea(12, "Hakkımda", '', "about",3, $row['about'], false,false);
                     getTextInput(12, "Meta Anahtar Kelimeler", "", "keywords", $row['keywords'], false, false);
                     getTextArea(12, "Meta Açıklama", "Açıklama", "description", 3, $row['description'], false, false);
                     getInputFile(5, "cv", "Cv Türkçe", false, false, false);
@@ -50,7 +50,7 @@ $row = getDataRow(1, 'about', $db);
                     <?php
                     getTextInput(4, "Title", "", "titleE", $row['titleE'], false, false);
                     getTextInput(5, "Link", "", "linkE", $row['linkE'], false, false);
-                    getTextArea(12, "Hakkımda", '', "aboutE", $row['aboutE'], false, false,false);
+                    getTextArea(12, "Hakkımda", '', "aboutE",3, $row['aboutE'], false, false);
                     getTextInput(12, "Meta Anahtar Kelimeler", "", "keywordsE", $row['keywordsE'], false, false);
                     getTextArea(12, "Meta Açıklama", "Açıklama", "descriptionE", 3, $row['descriptionE'], false, false);
                     getInputFile(5, "cvE", "Cv Türkçe", false, false, false);
