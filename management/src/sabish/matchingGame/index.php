ed469f8ad51388cd620b4b05a080aab9b0cf3bcb
rifaikuci <rifaikuci@gmail.com>
2023-12-10 11:55:05 +0300

    sabish

--- management/src/sabish/matchingGame/index.php
+++ management/src/sabish/matchingGame/index.php
@@ -63,7 +63,7 @@ else if (file_exists("../../../utils/index.php"))
     ?>
 
     <div class="content-wrapper">
-        <?php getBreadcrumb("Araç Listesi", ''); ?>
+        <?php getBreadcrumb("Eşleştirme Kategorileri", ''); ?>
         <?php require_once "main.php" ?>
     </div>
 
