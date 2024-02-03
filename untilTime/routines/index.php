4ab191f19e9715cbd53c39f4b17fb2ced52a040b
rifaikuci <rifaikuci@gmail.com>
2024-02-03 23:53:30 +0300

    percent fix

--- untilTime/routines/index.php
+++ untilTime/routines/index.php
@@ -43,3 +43,120 @@ if (isset($data['method']) && isset($data['method']) && $data['method'] == "save
         }
     }
 }
+
+if (isset($data['method']) && isset($data['method']) && $data['method'] == "getRoutinesMainPage") {
+
+    if ($deviceKey) {
+
+        try {
+            $deviceId = isset($data['deviceId']) ? $data['deviceId'] : null;
+
+            $sql = "select * from ". $tableUntilTimeRoutines ." where deviceId = '$deviceId' and isMainPage = 1 and isDeleted = 0";
+            $result = $db->query($sql);
+
+            $routines = array();
+            while ($row = $result->fetch_array()) {
+                $routine = null;
+                $routine['id'] = $row['id'];
+                $routine['title'] = $row['title'];
+                $routine['isMainPage'] = $row['isMainPage'];
+                $routine['isDeleted'] = $row['isDeleted'];
+                $routine['insertDate'] = dateWithTime($row['insertDate']);
+                $routine['updateDate'] = dateWithTime($row['updateDate']);
+
+                array_push($routines, $routine);
+            }
+
+            echo json_encode($routines);
+
+
+        } catch (Exception $exception) {
+            echo  json_encode(['success' => false]);
+        }
+    }
+}
+
+if (isset($data['method']) && isset($data['method']) && $data['method'] == "updateRoutineMainPage") {
+
+    if ($deviceKey) {
+
+        try {
+            $deviceId = isset($data['deviceId']) ? $data['deviceId'] : null;
+            $routineId = isset($data['routineId']) ? $data['routineId'] : null;
+
+            $sqlUpdate = "UPDATE ". $tableUntilTimeRoutines . " SET isMainPage = 0 WHERE deviceId = '$deviceId' AND id = '$routineId'";
+
+            echo $db->query($sqlUpdate) ? json_encode(['success' => true]) : json_encode(['success' => false]);
+
+        } catch (Exception $exception) {
+            echo  json_encode(['success' => false]);
+        }
+    }
+}
+
+if (isset($data['method']) && isset($data['method']) && $data['method'] == "deleteRoutine") {
+
+    if ($deviceKey) {
+
+        try {
+            $deviceId = isset($data['deviceId']) ? $data['deviceId'] : null;
+            $routineId = isset($data['routineId']) ? $data['routineId'] : null;
+
+            $sqlUpdate = "UPDATE ". $tableUntilTimeRoutines . " SET isDeleted = 1,isMainPage = 0  WHERE deviceId = '$deviceId' AND id = '$routineId'";
+
+
+            echo $db->query($sqlUpdate) ? json_encode(['success' => true]) : json_encode(['success' => false]);
+
+        } catch (Exception $exception) {
+            echo  json_encode(['success' => false]);
+        }
+    }
+}
+
+if (isset($data['method']) && isset($data['method']) && $data['method'] == "getRoutineId") {
+
+    if ($deviceKey) {
+
+        try {
+            $deviceId = isset($data['deviceId']) ? $data['deviceId'] : null;
+            $routineId = isset($data['routineId']) ? $data['routineId'] : null;
+
+            $sql = "select * from ". $tableUntilTimeRoutines ." where deviceId = '$deviceId' and id = '$routineId'";
+
+            $routine = getDataRow($routineId,$tableUntilTimeRoutines, $db);
+
+            echo json_encode([
+                'id' => $routine['id'],
+                'insertDate'=>  dateWithTime($routine['insertDate']) ,
+                'updateDate'=>  dateWithTime($routine['updateDate']) ,
+                'title'=>  $routine['title'],
+                'isMainPage'=>  $routine['isMainPage'],
+                ]);
+        } catch (Exception $exception) {
+            echo  json_encode(['success' => false]);
+        }
+    }
+}
+
+if (isset($data['method']) && isset($data['method']) && $data['method'] == "updateRoutine") {
+
+    if ($deviceKey) {
+
+        try {
+            $deviceId = isset($data['deviceId']) ? $data['deviceId'] : null;
+            $routineId = isset($data['routineId']) ? $data['routineId'] : null;
+
+            $title = isset($data['title']) ? $data['title'] : null;
+            $isMainPage = isset($data['isMainPage']) ? $data['isMainPage'] : 1;
+            $deviceId = getDataRowByColumn($deviceKey, $tableUntilTimeDevices, $db, "deviceKey")['id'];
+
+
+            $sqlUpdate = "UPDATE ". $tableUntilTimeRoutines . " SET title = '$title' , isMainPage = '$isMainPage'  WHERE id = '$routineId'";
+
+            echo $db->query($sqlUpdate) ? json_encode(['success' => true]) : json_encode(['success' => false]);
+
+        } catch (Exception $exception) {
+            echo  json_encode(['success' => false]);
+        }
+    }
+}
