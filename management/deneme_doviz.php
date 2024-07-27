f030d7409064059c03dfd4fdb7386f3dde8db78f
rifaikuci <rifaikuci@gmail.com>
2024-07-27 20:53:24 +0300

    api key add

--- management/deneme_doviz.php
+++ management/deneme_doviz.php
@@ -41,19 +41,19 @@ function getSleepDuration($hour, $day, $isHoliday)
 {
 
     if ($isHoliday) {
-        return 60 * 15; // 1 saat
+        return 60 * 60 * 2; // 1 saat
     }
 
     if ($day >= 1 && $day <= 5) { // Pazartesi-Cuma
         if ($hour >= 9 && $hour <= 19) {
-            return 60 * 5; // 1 saat
+            return 60 * 30; // 1 saat
         }
     } elseif ($day == 6) { // Cumartesi
         if ($hour >= 9 && $hour <= 14) {
-            return 60 * 5; // 1 saat
+            return 60 * 30; // 1 saat
         }
     }
-    return 60 * 15; // 1 saat
+    return 60 * 60 * 2; // 1 saat
 }
 
 function getActiveCurrencies($db)
