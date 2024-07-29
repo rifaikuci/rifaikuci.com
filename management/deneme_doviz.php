1a3b48becb1ec5f45c48dfed47a59d3d1e1b3dbc
rifaikuci <rifaikuci@gmail.com>
2024-07-30 00:21:04 +0300

    api key add

--- management/deneme_doviz.php
+++ management/deneme_doviz.php
@@ -174,7 +174,7 @@ function getDataRow2($id, $table, $db)
 
 $dollarApiKey = 1;
 $iterationCount = 0;
-$maxIterationsBeforeReconnect = 3; // Her 3 iterasyonda bir bağlantıyı yeniden başlat
+$maxIterationsBeforeReconnect = 2; // Her 3 iterasyonda bir bağlantıyı yeniden başlat
 
 $db = getDbConnection(); // Bağlantıyı başta aç
 
