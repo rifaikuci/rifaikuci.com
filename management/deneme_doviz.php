2c6b18d89661823a3800b2ecf742bc1f6f9b57e8
rifaikuci <rifaikuci@gmail.com>
2024-08-02 06:13:37 +0300

    api key add

--- management/deneme_doviz.php
+++ management/deneme_doviz.php
@@ -24,9 +24,8 @@ function getDbConnection()
     $db = new mysqli($host, $user, $password, $dbname);
 
     if ($db->connect_error) {
-        $hata = $db->connect_error;
-        logError("Bağlantı hatası: " . $hata);
-        die("Bağlantı başarısız: " . $db->connect_error);
+        logError("Connection error: " . $db->connect_error);
+        die("Connection failed: " . $db->connect_error);
     }
 
     return $db;
@@ -34,14 +33,8 @@ function getDbConnection()
 
 function ensureDbConnection($db)
 {
-    try {
-        if (!($db instanceof mysqli) || !$db->ping()) {
-            // $db geçerli bir mysqli nesnesi değilse veya bağlantısı aktif değilse, yeni bir bağlantı oluşturuyoruz
-            $db = getDbConnection();
-        }
-    } catch (Exception $ex) {
-        $hata  = $ex->getMessage();
-        logError("db coonnection kontrolü: " . $hata);
+    if (!($db instanceof mysqli) || !$db->ping()) {
+        $db = getDbConnection();
     }
 
     return $db;
@@ -50,35 +43,28 @@ function ensureDbConnection($db)
 function isHoliday($date, $db)
 {
     $sql = "SELECT COUNT(*) FROM holidays WHERE date = '$date'";
-    $result = mysqli_query($db, $sql);
+    $result = $db->query($sql);
     if (!$result) {
-        logError("isHoliday error: " . mysqli_error($db));
+        logError("isHoliday error: " . $db->error);
         return false;
     }
-    $row = mysqli_fetch_array($result);
+    $row = $result->fetch_array();
     return $row[0] > 0;
 }
 
 function getSleepDuration($hour, $day, $isHoliday)
 {
-    if ($isHoliday) {
-        return 60 * 30; // 30 dakika
+    if ($isHoliday || $day == 7) { // Holiday or Sunday
+        return 1800; // 30 minutes
     }
 
-    if ($day >= 1 && $day <= 5) { // Pazartesi-Cuma
-        if ($hour >= 9 && $hour <= 19) {
-            return 60 * 15; // 10 dakika
-        }
-    } elseif ($day == 6) { // Cumartesi
-        if ($hour >= 9 && $hour <= 14) {
-            return 60 * 15; // 10 dakika
-        }
-    } else {
-        return 60 * 30; // 30 dakika
+    if ($day >= 1 && $day <= 5 && $hour >= 9 && $hour <= 19) { // Monday-Friday 9-19
+        return 900; // 15 minutes
+    } elseif ($day == 6 && $hour >= 9 && $hour <= 14) { // Saturday 9-14
+        return 900; // 15 minutes
     }
 
-    // Default sleep duration if none of the conditions are met
-    return 60 * 30; // 30 dakika
+    return 1800; // Default 30 minutes
 }
 
 function getActiveCurrencies($db)
@@ -87,7 +73,7 @@ function getActiveCurrencies($db)
     $result = $db->query($sql);
 
     $activeCurrency = [];
-    while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+    while ($row = $result->fetch_assoc()) {
         if (strlen($row['code']) == 3) {
             $activeCurrency[] = [
                 'code' => $row['code'],
@@ -104,7 +90,7 @@ function getActiveGoldPrices($db)
     $result = $db->query($sql);
 
     $goldPrices = [];
-    while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+    while ($row = $result->fetch_assoc()) {
         if (strlen($row['code']) != 3) {
             $goldPrices[] = [
                 'code' => $row['code'],
@@ -115,17 +101,13 @@ function getActiveGoldPrices($db)
     return $goldPrices;
 }
 
-function fetchCurrencyDataFromApi($apiKey)
+function fetchDataFromApi($url, $apiKey)
 {
     $curl = curl_init();
     curl_setopt_array($curl, [
-        CURLOPT_URL => "https://api.collectapi.com/economy/allCurrency",
+        CURLOPT_URL => $url,
         CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
         CURLOPT_TIMEOUT => 30,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "GET",
         CURLOPT_HTTPHEADER => [
             "authorization: apikey " . $apiKey,
             "content-type: application/json"
@@ -140,147 +122,63 @@ function fetchCurrencyDataFromApi($apiKey)
     return $response;
 }
 
-function fetchGoldPricesDataFromApi($apiKey)
+function processAndInsertData($data, $activeItems, $db, $apiKey, $table)
 {
-    $curl = curl_init();
-    curl_setopt_array($curl, [
-        CURLOPT_URL => "https://api.collectapi.com/economy/goldPrice",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "GET",
-        CURLOPT_HTTPHEADER => [
-            "authorization: apikey " . $apiKey,
-            "content-type: application/json"
-        ],
-    ]);
-    $response = curl_exec($curl);
-    if (curl_errno($curl)) {
-        logError('Curl error: ' . curl_error($curl));
-    }
-    curl_close($curl);
-
-    return $response;
-}
-
-function processAndInsertCurrencies($currencies, $activeCurrency, $db, $dollarApiKey)
-{
-    $filteredCurrencies = array_map(function ($currency) use ($activeCurrency) {
-        $activeCodes = array_column($activeCurrency, 'code');
-        $activeIds = array_column($activeCurrency, 'id', 'code');
-        if (in_array($currency['code'], $activeCodes)) {
-            $currency['id'] = $activeIds[$currency['code']];
-            $currency['datetime'] = $currency['date'] . ' ' . $currency['time'];
-            unset($currency['date'], $currency['time']);
-            return $currency;
+    $filteredItems = array_filter(array_map(function ($item) use ($activeItems) {
+        $activeCodes = array_column($activeItems, 'code');
+        $activeIds = array_column($activeItems, 'id', 'code');
+        if (in_array($item['code'], $activeCodes)) {
+            $item['id'] = $activeIds[$item['code']];
+            $item['datetime'] = $item['date'] . ' ' . $item['time'];
+            unset($item['date'], $item['time']);
+            return $item;
         }
         return null;
-    }, $currencies);
+    }, $data));
 
-    $filteredCurrencies = array_filter($filteredCurrencies);
-
-    foreach ($filteredCurrencies as $row) {
-        $curCode = $row['id'];
-        $selling = $row['selling'];
-        $buying = $row['buying'];
-        $datetime = $row['datetime'];
-        $rate = $row['rate'];
-
+    foreach ($filteredItems as $row) {
         try {
-            $sql = "INSERT INTO currencyReponse (currencyCode, selling, buying, transactionDate, rate, apiKey)
-            VALUES ('$curCode', '$selling', '$buying', '$datetime','$rate', '$dollarApiKey')";
-            $result = mysqli_query($db, $sql);
+            $sql = "INSERT INTO $table (currencyCode, selling, buying, transactionDate, rate, apiKey)
+                    VALUES ('{$row['id']}', '{$row['selling']}', '{$row['buying']}', '{$row['datetime']}', '{$row['rate']}', '$apiKey')";
+            $db->query($sql);
         } catch (Exception $e) {
             logError($e->getMessage());
         }
     }
-
 }
 
-function processAndInsertGold($currencies, $activeCurrency, $db, $dollarApiKey)
-{
-    $filteredCurrencies = array_map(function ($currency) use ($activeCurrency) {
-        $activeCodes = array_column($activeCurrency, 'code');
-        $activeIds = array_column($activeCurrency, 'id', 'code');
-        if (in_array($currency['name'], $activeCodes)) {
-            $currency['id'] = $activeIds[$currency['name']];
-            $currency['datetime'] = $currency['date'] . ' ' . $currency['time'];
-            unset($currency['date'], $currency['time']);
-            return $currency;
-        }
-        return null;
-    }, $currencies);
-
-    $filteredCurrencies = array_filter($filteredCurrencies);
-
-    foreach ($filteredCurrencies as $row) {
-        $curCode = $row['id'];
-        $selling = $row['selling'];
-        $buying = $row['buying'];
-        $datetime = $row['datetime'];
-        $rate = $row['rate'];
-
-        try {
-            $sql = "INSERT INTO currencyReponse (currencyCode, selling, buying, transactionDate, rate, apiKey)
-            VALUES ('$curCode', '$selling', '$buying', '$datetime','$rate', '$dollarApiKey')";
-            $result = mysqli_query($db, $sql);
-        } catch (Exception $e) {
-            logError($e->getMessage());
-        }
-    }
-
-}
-
-function getDataRow2($id, $table, $db)
+function getDataRow($id, $table, $db)
 {
     $sql = "SELECT * FROM $table WHERE id = '$id'";
-    try {
-        $result =  mysqli_query($db, $sql);
-    }catch (Exception $e) {
-        logError($e->getMessage());
-    }
-
-
-    return mysqli_fetch_assoc($result);
+    return $db->query($sql)->fetch_assoc();
 }
 
 $dollarApiKey = 1;
 $goldApiKey = 1;
-
-$db = getDbConnection(); // Bağlantıyı başta aç
-
+$db = getDbConnection();
 $sumSecond = 0;
 
 while ($sumSecond < 3600) {
     try {
+        $currentHour = (int)date('G');
+        $currentDay = (int)date('N');
+        $currentDate = date('Y-m-d');
 
-        $currentHour = (int)date('G'); // Şu anki saat (0-23)
-        $currentDay = (int)date('N'); // Şu anki gün (1=Monday, 7=Sunday)
-        $currentDate = date('Y-m-d'); // Şu anki tarih
-
-        $db = ensureDbConnection($db); // Bağlantıyı kontrol et ve gerekirse yeniden bağlan
+        $db = ensureDbConnection($db);
 
         $isHoliday = isHoliday($currentDate, $db);
-        $dataFetched = false;
-        $dataFetchedGold = false;
+        $dataFetched = $dataFetchedGold = false;
 
         while (!$dataFetched) {
-            $dollarRow = getDataRow2($dollarApiKey, 'collectionApi', $db);
-
+            $dollarRow = getDataRow($dollarApiKey, 'collectionApi', $db);
             if ($dollarRow) {
-                $apiKey = $dollarRow['apiKey'];
-
-                $apiResponse = fetchCurrencyDataFromApi($apiKey);
-
+                $apiResponse = fetchDataFromApi("https://api.collectapi.com/economy/allCurrency", $dollarRow['apiKey']);
                 $response = json_decode($apiResponse, true);
-
                 if ($response && $response['success']) {
-                    processAndInsertCurrencies($response['result'], getActiveCurrencies($db), $db, $dollarApiKey);
-                    $dataFetched = true; // Veri başarıyla alındı, döngüyü kır
+                    processAndInsertData($response['result'], getActiveCurrencies($db), $db, $dollarApiKey, 'currencyReponse');
+                    $dataFetched = true;
                 } else {
-                    $dollarApiKey += 1; // API anahtarını arttır
+                    $dollarApiKey++;
                 }
             } else {
                 $dollarApiKey = 1;
@@ -288,35 +186,29 @@ while ($sumSecond < 3600) {
         }
 
         while (!$dataFetchedGold) {
-            $goldRow = getDataRow2($goldApiKey, 'collectionApi', $db);
-
+            $goldRow = getDataRow($goldApiKey, 'collectionApi', $db);
             if ($goldRow) {
-                $goldKey = $goldRow['apiKey'];
-
-                $apiResponseGold = fetchGoldPricesDataFromApi($goldKey);
-
-                $responsegGold = json_decode($apiResponseGold, true);
-
-                if ($responsegGold && $responsegGold['success']) {
-                    processAndInsertGold($responsegGold['result'], getActiveGoldPrices($db), $db, $goldApiKey);
-                    $dataFetchedGold = true; // Veri başarıyla alındı, döngüyü kır
+                $apiResponseGold = fetchDataFromApi("https://api.collectapi.com/economy/goldPrice", $goldRow['apiKey']);
+                $responseGold = json_decode($apiResponseGold, true);
+                if ($responseGold && $responseGold['success']) {
+                    processAndInsertData($responseGold['result'], getActiveGoldPrices($db), $db, $goldApiKey, 'currencyReponse');
+                    $dataFetchedGold = true;
                 } else {
-                    $dataFetchedGold += 1; // API anahtarını arttır
+                    $goldApiKey++;
                 }
             } else {
                 $goldApiKey = 1;
             }
         }
 
-
-
         $sleepDuration = getSleepDuration($currentHour, $currentDay, $isHoliday);
-        $sumSecond = $sumSecond + $sleepDuration;
+        $sumSecond += $sleepDuration;
         sleep($sleepDuration);
     } catch (Exception $e) {
-    } finally {
+        logError($e->getMessage());
     }
 }
 
-// Betik sona ermeden önce bağlantıyı kapat
+$db->close();
+
 ?>
