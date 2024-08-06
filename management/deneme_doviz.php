1e971a003c4aa9e6872b32bf7a2580d6f6eeb51c
rifaikuci <rifaikuci@gmail.com>
2024-08-06 08:52:53 +0300

    buyyingY eklendi.

--- management/deneme_doviz.php
+++ management/deneme_doviz.php
@@ -28,9 +28,9 @@ function logError($message)
 function getDbConnection()
 {
     $host = 'localhost';
-    $user = 'root';
-    $password = '';
-    $dbname = 'rifaikuci';
+    $user = 'rifaikuc';
+    $password = 'Gt36wwY2x7';
+    $dbname = 'rifaikuc_rifaikuci';
 
     $db = new mysqli($host, $user, $password, $dbname);
 
